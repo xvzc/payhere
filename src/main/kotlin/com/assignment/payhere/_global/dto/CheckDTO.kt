1a7ac011023e1304ef7495ddf8d6170ff6a25b2a
jerry901 <pkhu2000@gmail.com>
2021-11-26 03:05:41 +0900

    [update] renamed CheckResponse.kt to CheckDTO.kt

--- src/main/kotlin/com/assignment/payhere/_global/dto/CheckDTO.kt
+++ src/main/kotlin/com/assignment/payhere/_global/dto/CheckDTO.kt
@@ -2,7 +2,6 @@ package com.assignment.payhere._global.dto
 
 import com.assignment.payhere._global.error.BasicError
 
-data class CheckResponse(
+data class CheckDTO(
     val success: Boolean,
-    val error: BasicError? = null
 )
