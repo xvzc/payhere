3baf17e11c698edb0918e4690d9743c0916bfc00
jerry901 <pkhu2000@gmail.com>
2021-11-26 03:06:35 +0900

    [update] database init.sql, my.cnf

--- db/initdb.d/01-init.sql
+++ db/initdb.d/01-init.sql
@@ -2,12 +2,32 @@ CREATE DATABASE IF NOT EXISTS `payhere`;
 USE `payhere`;
 
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `receipt`;
+DROP TABLE IF EXISTS `tag`;
 
 CREATE TABLE IF NOT EXISTS `user` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `email`      VARCHAR(512) NOT NULL DEFAULT '',
-    `password`    VARCHAR(512)  NOT NULL DEFAULT '',
-    `created` DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `email`     VARCHAR(191) NOT NULL,
+    `password`  VARCHAR(512) NOT NULL,
+    `created`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     PRIMARY KEY (`id`),
     UNIQUE (`email`)
-    ) ENGINE = InnoDB;
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `receipt` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `user_id`   BIGINT NOT NULL,
+    `amount`    INT NOT NULL,
+    `created`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `tag` (
+   `id`   BIGINT NOT NULL AUTO_INCREMENT,
+   `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
